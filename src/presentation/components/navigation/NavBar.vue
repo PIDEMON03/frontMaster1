2b49eb739ae83dd1f48bf782183532cff86c86ef
root <root@PC-MEHDY>
2024-07-31 20:29:55 +0200

    Ajout page accueil + bootstrap

---
+++ src/presentation/components/navigation/NavBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <nav class="navbar navbar-dark">
+        <div class="container-fluid">
+            <RouterLink to="/">
+                <span class="navbar-brand mb-0 h1">
+                    Universite
+                </span>
+            </RouterLink>
+        </div>
+    </nav>
+</template>
